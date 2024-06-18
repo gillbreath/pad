0b64c7c3969ca033be17eb2ac110e9be781c0f98
Elias <elias@eliasjames.com>
2024-06-18 14:46:56 -0400

    Start CRUD routes for data entities

--- vue-scaffold/src/router/index.js
+++ vue-scaffold/src/router/index.js
@@ -3,6 +3,8 @@ import Renderer from '../components/Renderer.vue'
 import mainPad from '../../../main.pad.js'
 
 const routes = [];
+
+// pageRoutes
 Object.entries(mainPad.pageRoutes).forEach(pageRoute => {
   const [pageRouteKey, pageRouteValue] = pageRoute;
   routes.push({
@@ -13,6 +15,29 @@ Object.entries(mainPad.pageRoutes).forEach(pageRoute => {
     },
   });
 });
+
+// CRUD routes for each dataEntity
+Object.entries(mainPad.dataEntities).forEach(dataEntity => {
+  const [dataEntityKey, dataEntityValue] = dataEntity;
+  routes.push({
+    path: '/data-entities/' + dataEntityKey,
+    component: Renderer,
+    props: {
+      elementsArray: [
+        {
+          elementType: 'h1',
+          innerHtml: dataEntityKey,
+        },
+        {
+          elementType: 'dataEntity',
+          dataEntity: dataEntityKey,
+          renderType: 'ul'
+        }
+      ]
+    }
+  });
+});
+
 
 const router = createRouter({
   history: createWebHistory(),
