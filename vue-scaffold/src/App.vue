040df1e0c326bba10c07a62f8734e9c9ca7f3a5f
Elias <elias@eliasjames.com>
2024-08-08 13:29:40 -0400

    Mkae pre fab header global

--- vue-scaffold/src/App.vue
+++ vue-scaffold/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import mainPad from '../../main.pad.js';
+import PreFabHeader from './components/PreFab/PreFabHeader.vue';
 let showCrudControls = false;
 if (mainPad.dataEntities) showCrudControls = true;
 </script>
 
 <template>
-  <RouterView />
+  <template v-if="mainPad?.padOptions?.header">
+    <PreFabHeader :options="mainPad.padOptions.header.options" />
+  </template>
+  <main>
+    <section>
+      <div class="well">
+        <RouterView />
+      </div>
+    </section>
+  </main>
   <template v-if="showCrudControls">
     <div id="crud-controls">
       <RouterLink :to="'/'">home</RouterLink>
