5651821825c76de919ce3a0d57ba1e9277130fcc
Elias <elias@eliasjames.com>
2024-08-22 16:13:21 -0400

    Update PAD example to current API

--- pad/blog.example.pad.js
+++ pad/blog.example.pad.js
@@ -1,9 +1,13 @@
 const dataEntities = {
   articles: {
     fields: {
-      author: {},
       category: {},
-      text: {},
+      slug: {
+        defaultValue: 'unique-and-url-formatted'
+      },
+      text: {
+        defaultValue: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      },
       title: {},
     },
     preLoad: [
