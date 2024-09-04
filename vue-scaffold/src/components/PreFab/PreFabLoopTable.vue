604018b88c47dad9186ec7c3a6aa1d76e6c0a779
Elias <elias@eliasjames.com>
2024-09-04 12:09:13 -0400

    Non-CRUD mode for Table

--- vue-scaffold/src/components/PreFab/PreFabLoopTable.vue
+++ vue-scaffold/src/components/PreFab/PreFabLoopTable.vue
@@ -6,23 +6,24 @@ const props = defineProps({
   collection: Array,
   options: Object
 });
-
-const tableSchema = [];
+const tableSchema = props.options.displayColumns || [];
 if (props.collection) {
   props.collection.forEach((e) => {
     e.loopKey = LoopKey();
-    Object.keys(e).forEach((eachCollectionObjectKey) => {
-      if (tableSchema.indexOf(eachCollectionObjectKey) < 0) {
-        tableSchema.push(eachCollectionObjectKey);
-      }
-    });
+    if (!props.options.displayColumns) {
+      Object.keys(e).forEach((eachCollectionObjectKey) => {
+        if (tableSchema.indexOf(eachCollectionObjectKey) < 0) {
+          tableSchema.push(eachCollectionObjectKey);
+        }
+      });
+    }
   });
 }
 </script>
 
 <template>
-  <table border="1">
-    <tr>
+  <table :border="props.options.crudMode === true ? 1 : 0">
+    <tr v-if="props.options.crudMode === true">
       <!-- eslint-disable-next-line vue/require-v-for-key -->
       <th v-for="eachField in tableSchema.filter((e) => e !== 'loopKey')">
         {{ eachField }}
@@ -30,7 +31,7 @@ if (props.collection) {
     </tr>
     <tr v-for="(eachItem, index) in props.collection" :key="eachItem.loopKey">
       <!-- eslint-disable-next-line vue/require-v-for-key -->
-      <td v-for="eachField in Object.keys(eachItem).filter((e) => e !== 'loopKey')">
+      <td v-for="eachField in tableSchema.filter((e) => e !== 'loopKey')">
         {{
           typeof eachItem[eachField] === 'string'
             ? eachItem[eachField].substr(0, 20) + (eachItem[eachField].length > 20 ? '...' : '')
