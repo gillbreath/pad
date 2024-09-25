01e8a10e190254d9cd8810e890f08734ac0530fb
Elias <elias@eliasjames.com>
2024-09-25 16:48:51 -0400

    Add Find and Update actions to DataRenderer

--- vue-scaffold/src/components/DataEntity/DataRenderer.vue
+++ vue-scaffold/src/components/DataEntity/DataRenderer.vue
@@ -3,6 +3,8 @@ import PreFabLoopUL from '@/components/PreFab/PreFabLoopUL.vue';
 import PreFabLoopTable from '@/components/PreFab/PreFabLoopTable.vue';
 import useGlobalStores from '@/stores/GlobalStores.js';
 import CrudCreate from '@/components/DataEntity/CrudCreate.vue';
+import CrudFind from '@/components/DataEntity/CrudFind.vue';
+import CrudUpdate from '@/components/DataEntity/CrudUpdate.vue';
 
 const props = defineProps({
   options: Object
@@ -16,7 +18,7 @@ if (props.options.collectionSort) {
 
 <template>
   <PreFabLoopUL
-    v-if="!props.options.renderType"
+    v-if="!props.options.renderType || props.options.renderType === 'ul'"
     :collection="myStore.collection"
     :options="props.options"
     v-slot:default="slotProps"
@@ -30,8 +32,19 @@ if (props.options.collectionSort) {
   >
   </PreFabLoopTable>
   <CrudCreate
-    :data-entity-key="props.options.dataEntityKey"
     v-if="props.options.renderType === 'create'"
+    :data-entity-key="props.options.dataEntityKey"
   >
   </CrudCreate>
+  <CrudFind
+    v-if="props.options.renderType === 'find'"
+    :data-entity-key="props.options.dataEntityKey"
+    :crud-schema="props.options.children"
+  >
+  </CrudFind>
+  <CrudUpdate
+    v-if="props.options.renderType === 'update'"
+    :data-entity-key="props.options.dataEntityKey"
+  >
+  </CrudUpdate>
 </template>
