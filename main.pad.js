690085a289aab0cc837e397e3e5c40355aecc077
Elias <elias@eliasjames.com>
2024-10-15 11:23:54 -0400

    Improve example name

--- main.pad.js
+++ main.pad.js
@@ -1,4 +1,4 @@
-import seedPad from './pad/examples/03-logins-layouts-wizards.example.pad.js';
+import seedPad from './pad/examples/02-blog-crud-data.example.pad.js';
 import ElementRendererTests from './pad/ElementRenderer.tests.pad.js';
 
 // testing
